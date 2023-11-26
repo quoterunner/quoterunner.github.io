0a50ab865d08b176102b674c51273ddc5ae170ea
quoterunner <152044541+quoterunner@users.noreply.github.com>
2023-11-25 23:03:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,194 @@
+function displayError(icon, text) {
+  document.getElementById("local-error-trigger").innerHTML =
+    '<div id="local-error-container"> <div id="local-error"> <span class="material-symbols-rounded" style="font-size: 50px;"> ' +
+    icon +
+    " </span> <h2> " +
+    text +
+    " </h2> </div> </div>";
+}
+
+function clearError() {
+  document.getElementById("local-error-trigger").innerHTML = "";
+}
+
+var seconds = 0;
+var quoteLength = 0;
+var timerRunning = false;
+function timer() {
+  if (timerRunning) {
+    seconds++;
+    setTimeout(timer, 1000);
+  }
+}
+
+function getQuote(minLength, maxLength, lengthName) {
+  firstLetter = true;
+  currentLetterKeypress = 0;
+  correctLetters = 0;
+
+  if (
+    minLength == undefined ||
+    maxLength == undefined ||
+    lengthName == undefined
+  ) {
+    var minLength = localStorage.getItem("minLength");
+    var maxLength = localStorage.getItem("maxLength");
+    var lengthName = localStorage.getItem("lengthName");
+  }
+
+  localStorage.setItem("minLength", minLength);
+  localStorage.setItem("maxLength", maxLength);
+  localStorage.setItem("lengthName", lengthName);
+
+  document.getElementById("small").style.color = "black";
+  document.getElementById("medium").style.color = "black";
+  document.getElementById("large").style.color = "black";
+  document.getElementById("save-your-soul").style.color = "black";
+
+  document.getElementById(lengthName).style.color = "blue";
+
+  // quote api from https://github.com/lukePeavey/quotable
+  fetch(
+    "https://api.quotable.io/quotes/random?minLength=" +
+      minLength +
+      "&maxLength=" +
+      maxLength
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      var json = json[0];
+      var quote = json["content"].split("");
+      var author = "-" + json["author"];
+
+      console.log(" ");
+      console.info("New quote: " + json["content"] + " " + author);
+
+      const quoteElement = document.getElementById("quote");
+      const authorElement = document.getElementById("author");
+
+      quoteElement.innerHTML = "";
+      quoteLength = json["length"];
+
+      var currentQuoteIndex = 0;
+      while (currentQuoteIndex < quoteLength) {
+        var character = quote[currentQuoteIndex];
+        if (character == " ") {
+          character = "&nbsp;";
+        }
+
+        var h1 = document.createElement("h1");
+        h1.innerHTML = character;
+        h1.id = currentQuoteIndex;
+        quoteElement.appendChild(h1);
+
+        currentQuoteIndex++;
+      }
+      authorElement.innerHTML = author;
+    });
+}
+
+document.addEventListener("keydown", (event) => {
+  var caps = event.getModifierState("CapsLock");
+  if (caps) {
+    displayError("keyboard_capslock", "Caps lock on");
+  } else {
+    clearError();
+  }
+});
+
+document.addEventListener("keypress", (event) => keypressEvent(event));
+
+var currentLetterKeypress = 0;
+var correctLetters = 0;
+var firstLetter = true;
+var enterAllowed = false;
+function keypressEvent(event) {
+  var key = event.key;
+
+  if (enterAllowed == true && key == "Enter") {
+    getQuote();
+    enterAllowed == false;
+    return;
+  }
+
+  if (firstLetter == true) {
+    firstLetter = false;
+    seconds = 0;
+    timerRunning = true;
+    timer();
+  }
+
+  var currentLetter = document.getElementById(currentLetterKeypress);
+  if (key == " ") {
+    key = "&nbsp;";
+  }
+
+  if (currentLetter.innerHTML == "…") {
+    if (key == ".") {
+      key = "…";
+    }
+  }
+
+  if (key == currentLetter.innerHTML) {
+    currentLetter.style.color = "green";
+    currentLetter.style.borderLeft = "";
+    currentLetterKeypress++;
+    correctLetters++;
+
+    currentLetter = document.getElementById(currentLetterKeypress);
+    if (currentLetter == null) {
+      timerRunning = false;
+      var accuracy = Math.round((correctLetters / quoteLength) * 100);
+      var rawWpm = Math.round(quoteLength / 5 / (seconds / 60));
+      var wpm = Math.round(rawWpm * (accuracy / 100));
+
+      console.log("Quote stats: ");
+      console.info("Quote length: " + quoteLength);
+      console.info("Correct letters: " + correctLetters);
+      console.info("Seconds to complete: " + seconds);
+      console.info("Raw WPM: " + rawWpm);
+      console.info("Accuracy: " + accuracy);
+      console.info("WPM: " + wpm);
+
+      document.getElementById("quote").innerHTML =
+        "<div id='results'><h1>" +
+        wpm +
+        "</h1><p>WPM</p></div><div id='results'><h1>" +
+        accuracy +
+        "%</h1><p>Accuracy</p></div><div id='results'><h1>" +
+        rawWpm +
+        "</h1><p>Raw WPM</p></div><div id='results'><h1>" + seconds + "</h1><p>Seconds</p></div><div id='results'><a onclick='getQuote();'><span class='material-symbols-rounded' style='color: black; font-size: 50px;'>start</span></a></div>";
+      enterAllowed = true;
+
+      firstLetter = true;
+      currentLetterKeypress = 0;
+      correctLetters = 0;
+    } else {
+      currentLetter.style.borderLeft = "3px solid black";
+      if (document.getElementById(currentLetterKeypress + 10) !== null) {
+        document
+          .getElementById(currentLetterKeypress + 10)
+          .scrollIntoView(true);
+      }
+    }
+  } else {
+    currentLetter.style.borderLeft = "3px solid red";
+    correctLetters--;
+  }
+}
+
+var minLength = localStorage.getItem("minLength");
+var maxLength = localStorage.getItem("maxLength");
+var lengthName = localStorage.getItem("lengthName");
+
+if (
+  minLength == undefined ||
+  maxLength == undefined ||
+  lengthName == undefined
+) {
+  minLength = 0;
+  maxLength = 50;
+  lengthName = "small";
+}
+
+getQuote(minLength, maxLength, lengthName);
