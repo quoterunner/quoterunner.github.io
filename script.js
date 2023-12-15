c247998786c4cb2ee809df8c05080e95c7a60656
quoterunner <peytonmehl1@icloud.com>
2023-12-15 17:52:23 -0600

    added an invalid score alert

--- script.js
+++ script.js
@@ -30,6 +30,8 @@ var mediumSize = document.getElementById("medium");
 var largeSize = document.getElementById("large");
 var saveYourSoulSize = document.getElementById("save-your-soul");
 
+var invalidScoreAlert = "Invalid score, accuracy below 0";
+
 //for themes
 var headerQuoteUnselected;
 var headerQuoteSelected;
@@ -98,6 +100,10 @@ function getQuote(minLength, maxLength, lengthName) {
   currentLetterKeypress = 0;
   correctLetters = 0;
 
+  if(document.getElementById("local-error-container").innerHTML == invalidScoreAlert){
+    clearError();
+  }
+
   if (
     minLength == undefined ||
     maxLength == undefined ||
@@ -118,7 +124,7 @@ function getQuote(minLength, maxLength, lengthName) {
   largeSize.style.color = headerQuoteUnselected;
   saveYourSoulSize.style.color = headerQuoteUnselected;
 
-  if(document.getElementById(lengthName) == null){
+  if (document.getElementById(lengthName) == null) {
     minLength = 0;
     maxLength = 50;
     lengthName = "small";
@@ -160,7 +166,7 @@ function getQuote(minLength, maxLength, lengthName) {
         currentQuoteIndex++;
       }
       authorElement.innerHTML = author;
-    })
+    });
 }
 
 function keypressEvent(event) {
@@ -169,7 +175,7 @@ function keypressEvent(event) {
   if (key == "Enter" || key == "Tab") {
     getQuote();
     return;
-  } else if (key == "Shift" || key == "CapsLock"){
+  } else if (key == "Shift" || key == "CapsLock") {
     return;
   }
 
@@ -204,31 +210,37 @@ function keypressEvent(event) {
       if (currentLetter == null) {
         timerRunning = false;
         var accuracy = Math.round((correctLetters / quoteLength) * 100);
-        var rawWpm = Math.round(quoteLength / 5 / (seconds / 60));
-        var wpm = Math.round(rawWpm * (accuracy / 100));
+        if (accuracy < 0) {
+          displayError(invalidScoreAlert);
+        } else {
+          var rawWpm = Math.round(quoteLength / 5 / (seconds / 60));
+          var wpm = Math.round(rawWpm * (accuracy / 100));
 
-        console.log("Quote stats: ");
-        console.info("Quote length: " + quoteLength);
-        console.info("Correct letters: " + correctLetters);
-        console.info("Seconds to complete: " + seconds);
-        console.info("Raw WPM: " + rawWpm);
-        console.info("Accuracy: " + accuracy);
-        console.info("WPM: " + wpm);
+          console.log("Quote stats: ");
+          console.info("Quote length: " + quoteLength);
+          console.info("Correct letters: " + correctLetters);
+          console.info("Seconds to complete: " + seconds);
+          console.info("Raw WPM: " + rawWpm);
+          console.info("Accuracy: " + accuracy);
+          console.info("WPM: " + wpm);
 
-        document.getElementById("quote").innerHTML =
-          "<div id='results'><h1>" +
-          wpm +
-          "</h1><p>WPM</p></div><div id='results'><h1>" +
-          accuracy +
-          "%</h1><p>Accuracy</p></div><div id='results'><h1>" +
-          rawWpm +
-          "</h1><p>Raw WPM</p></div><div id='results'><h1>" +
-          seconds +
-          "</h1><p>Seconds</p></div><div id='results'><svg onclick='getQuote();' fill='" + nextColor + "' xmlns='http://www.w3.org/2000/svg' height='48' viewBox='0 -960 960 960' width='48'><path d='M104.509-225q-20.14 0-32.825-13.113Q59-251.225 59-270v-420q0-19.2 12.86-32.6 12.86-13.4 33-13.4Q124-736 137-722.6t13 32.6v420q0 18.775-13.175 31.887Q123.649-225 104.509-225ZM727-435H295q-20.75 0-33.375-13.158t-12.625-32Q249-499 261.625-512.5T295-526h432L608-647q-15-13-14.5-31.8T608-711q13.289-13 31.144-13Q657-724 672-711l197 198q6 6.16 10.5 15.313 4.5 9.154 4.5 17.9 0 8.747-4.5 17.767T869-448L672-250q-15.25 14-33.283 14.5t-30.875-12.717Q593-261 593-280.767q0-19.766 14-34.233l120-120Z'/></svg></div>";
+          document.getElementById("quote").innerHTML =
+            "<div id='results'><h1>" +
+            wpm +
+            "</h1><p>WPM</p></div><div id='results'><h1>" +
+            accuracy +
+            "%</h1><p>Accuracy</p></div><div id='results'><h1>" +
+            rawWpm +
+            "</h1><p>Raw WPM</p></div><div id='results'><h1>" +
+            seconds +
+            "</h1><p>Seconds</p></div><div id='results'><svg onclick='getQuote();' fill='" +
+            nextColor +
+            "' xmlns='http://www.w3.org/2000/svg' height='48' viewBox='0 -960 960 960' width='48'><path d='M104.509-225q-20.14 0-32.825-13.113Q59-251.225 59-270v-420q0-19.2 12.86-32.6 12.86-13.4 33-13.4Q124-736 137-722.6t13 32.6v420q0 18.775-13.175 31.887Q123.649-225 104.509-225ZM727-435H295q-20.75 0-33.375-13.158t-12.625-32Q249-499 261.625-512.5T295-526h432L608-647q-15-13-14.5-31.8T608-711q13.289-13 31.144-13Q657-724 672-711l197 198q6 6.16 10.5 15.313 4.5 9.154 4.5 17.9 0 8.747-4.5 17.767T869-448L672-250q-15.25 14-33.283 14.5t-30.875-12.717Q593-261 593-280.767q0-19.766 14-34.233l120-120Z'/></svg></div>";
 
-        firstLetter = true;
-        currentLetterKeypress = 0;
-        correctLetters = 0;
+          firstLetter = true;
+          currentLetterKeypress = 0;
+          correctLetters = 0;
+        }
       } else {
         currentLetter.style.borderLeft = "3px solid " + cursorColor;
         if (document.getElementById(currentLetterKeypress + 10) !== null) {
